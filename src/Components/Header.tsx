5619ff6f21330f5f1996fbc7034037e1bfbe02f7
Digen More <digenmore4@gmail.com>
2023-03-04 14:17:45 +0530

    Animation Added in header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,6 +7,23 @@ function Header() {
         <div className="--logo">
           <img src="/logo.png" alt="" />
         </div>
+        <div className="--img-container">
+          <img className="--img" src="/captain-america.png" alt="" />
+          <img className="--img" src="/ironman.png" alt="" />
+          <img className="--img" src="/thor.png" alt="" />
+          <img className="--img" src="/panther.png" alt="" />
+        </div>
+        <div className="--search-container">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search Movie Here..."
+          />
+          <span>
+            <i className="uil uil-search search"></i>
+          </span>
+        </div>
       </nav>
     </>
   );
